d0d331b9190b3d46b78a41063ac14692c5962e1b
Aman katiyar <amankatiyar783597@gmail.com>
2024-01-23 23:14:32 +0530

    prettier added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express')
+import express  from 'express'
 const app = express()
-require('dotenv').config()
-// const port = 3000
+// require('dotenv').config()
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -15,10 +15,15 @@ app.post('./register' , (req , res) => {
     res.send("user register sucessfully")
 })
 
+app.get('/hi' , (req , res) =>{
+    const hi = [{1 :"hi aman"} , {2 : "hi janvi"} , {3 : "hi everyone"}]
+    res.send(hi)
+})
+
 app.get('/getelement' ,(req , res) =>{
     res.send("<h1>hi this is Aman katiyar</h1>")
 })
 
-app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${process.env.PORT}`)
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
